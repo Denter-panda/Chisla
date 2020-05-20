787bbf729ee38ab95a38971c32078391a82ad90a
Denter-panda <65632080+Denter-panda@users.noreply.github.com>
2020-05-20 23:39:31 +0500

    Add files via upload

---
+++ Подсчёт количества букв в тексте.cpp
@@ -0,0 +1,26 @@
+﻿// Подсчёт количества букв в тексте Погнаев Д.С.
+
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    char str[200];
+    std::cout << "Введите строку: ";
+    std::cin.get(str, 200);
+
+    int schet = 0;
+    int len = strlen(str);
+    int a1 = 0;
+    for (int i = 0; i < len; i++)
+    {
+        if (str[i] != ' ')
+            schet++;
+        if (str[i] == 'r')
+            a1++;
+    }
+    std::cout <<"Частота буквы r в тексте: "<< a1 << std::endl;
+    std::cout << "Количество букв в тексте = " << schet << std::endl;
+
+    return 0;
+}
